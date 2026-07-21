40f73d2baa7ed230041d59de09f912e3e452d339
oth <oth1118@naver.com>
2026-07-21 23:28:33 +0900

    delete

--- db/init/02-seed.sql
+++
@@ -1,8 +0,0 @@
--- DB Tier: initial movie data
-INSERT INTO movies (title, runtime, genre, rating, poster_color, poster_url) VALUES
-  ('듄: 파트 2', 166, 'SF/액션', 8.5, '#c45c26', '/posters/dune2.jpg'),
-  ('오펜하이머', 180, '드라마', 8.3, '#1a3a5c', '/posters/oppenheimer.jpg'),
-  ('범죄도시 4', 109, '액션', 7.2, '#8b1a1a', '/posters/roundup4.jpg'),
-  ('인사이드 아웃 2', 96, '애니메이션', 8.0, '#4a90a4', '/posters/insideout2.jpg'),
-  ('파묘', 134, '미스터리', 7.8, '#2d4a2d', '/posters/exhuma.jpg'),
-  ('쿵푸팬더 4', 94, '애니메이션', 6.9, '#d4a017', '/posters/kungfupanda4.jpg');
